b674e788536b19b416b27608514afff35fb5ba7c
Oriol Morros Vilaseca <OM368@student.aru.ac.uk>
2026-04-18 16:55:18 +0100

    fix: add TURN server to WebRTC ICE config so call connects across NATs

--- frontend/lib/useVideoCall.ts
+++ frontend/lib/useVideoCall.ts
@@ -30,6 +30,15 @@ const INITIAL: VideoCallState = {
 const ICE_SERVERS: RTCIceServer[] = [
   { urls: "stun:stun.l.google.com:19302" },
   { urls: "stun:stun1.l.google.com:19302" },
+  {
+    urls: [
+      "turn:openrelay.metered.ca:80",
+      "turn:openrelay.metered.ca:443",
+      "turn:openrelay.metered.ca:443?transport=tcp",
+    ],
+    username: "openrelayproject",
+    credential: "openrelayproject",
+  },
 ];
 
 type Opts = {
